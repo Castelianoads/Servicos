b78a518fe6e4102148802281b2fac03a8c3dfb42
Carlos Eduardo Casteliano <carlosecdpaula@gmail.com>
2025-07-19 01:04:51 -0300

    feat: Implementação do swagger

--- DownloadMusica/Program.cs
+++ DownloadMusica/Program.cs
@@ -2,12 +2,19 @@ using DownloadMusica.Interfaces.Services;
 using DownloadMusica.Models;
 using DownloadMusica.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Baixar musica API", Version = "v1" });
+});
+
 builder.Services.AddTransient<IYoutubeService, YoutubeService>();
 
 var app = builder.Build();
@@ -15,8 +22,16 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+   
 }
+
+app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Baixar musica API V1");
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 
